6022f5f7ac8fd6d9e5fe40815210c7a9aa2f9af1
Ninoorta <blackcathp9x@gmail.com>
2021-11-03 17:21:06 +0700

    use the new form of creating new stripe checkout session

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -53,7 +53,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email }))._id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.line_items[0].amount_total / 100;
   console.log('run to this');
   console.log(user);
   await Booking.create({ tour, user, price });
